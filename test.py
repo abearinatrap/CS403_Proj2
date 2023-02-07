b20623a8a151f4012f776f2b5cfddd302555e5b9
EJYohannan <99680407+EJYohannan@users.noreply.github.com>
2023-02-07 15:06:14 -0600

    stuff

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import algviz
 # Create a visualizer object.
-# The default duration of each frame of animation is 1.0 second.
+# The default duration of each frame of animation is 3.0 seconds.
 # The interval between two frames of animation is 0.5 seconds.
-viz = algviz.Visualizer(delay=1.0, wait=0.5)
+viz = algviz.Visualizer(delay=3.0, wait=0.5)
 
 viz.display()               # Display the firt animation frame.
 # Do something...
-viz.display(delay=1.0)      # Display the second animation frame.
+viz.display(delay=2.0)      # Display the second animation frame.
 
 tab = viz.createTable(
     row=3,                    # The table has 3 rows.
@@ -18,14 +18,14 @@ tab = viz.createTable(
 )
 viz.display()
 
-# curNum = input("Enter a number: ")
-# tab[1][0] = int(curNum)
-# viz.display(0.5)
-# curNum = input("Choose an index to flash green: ")
-# tab.mark(algviz.color_green, int(curNum), 0)
-# viz.display(0.5)
-# tab.removeMark(algviz.color_green)
-# viz.display(0.5)
-# print('time to resize')
-# tab.reshape(row=3, col=2)  # Change the table shape to (row:2, col:2).
-# viz.display(1)
+curNum = input("Enter a number: ")
+tab[1][0] = int(curNum)
+viz.display(0.5)
+curNum = input("Choose an index to flash green: ")
+tab.mark(algviz.color_green, int(curNum), 0)
+viz.display(0.5)
+tab.removeMark(algviz.color_green)
+viz.display(0.5)
+print('time to resize')
+tab.reshape(row=3, col=2)  # Change the table shape to (row:2, col:2).
+viz.display(1)
